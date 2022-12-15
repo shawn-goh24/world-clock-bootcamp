41de57f04a51a9607f85b04d9f1ef44aa9daf8e4
shawn-goh24 <ahshawngoh@gmail.com>
2022-12-15 22:43:37 +0800

    Added feature to remove existing clock by clicking the selection again

--- src/App.js
+++ src/App.js
@@ -5,20 +5,49 @@ import "./App.css";
 import WorldClock from "./Components/WorldClock";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      timeZone: []
+    }
+  }
+
+  addTimeZone(zone) {
+    if (!this.state.timeZone.includes(zone)) {
+      this.setState({
+        timeZone: [...this.state.timeZone, zone]
+      })
+    } else {
+      this.setState({
+        timeZone: this.state.timeZone.filter((i) => { return i !== zone })
+      });
+    }
+  }
+
+
   render() {
-    const clockData = [
-      "America/Los_Angeles",
-      "Europe/London",
-      "Asia/Singapore",
-      "Africa/Lagos"
-    ]
+    // const clockData = [
+    //   "America/Los_Angeles",
+    //   "Europe/London",
+    //   "Asia/Singapore",
+    //   "Africa/Lagos"
+    // ]
+    const clickDemo = (e) => {
+      this.addTimeZone(e.target.innerText)
+    }
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-
-          <WorldClock clockData={clockData}/>
+          <select multiple>
+            <option onClick={clickDemo} key="America/Los_Angeles">America/Los_Angeles</option>
+            <option onClick={clickDemo} key="Europe/London">Europe/London</option>
+            <option onClick={clickDemo} key="Asia/Singapore">Asia/Singapore</option>
+            <option onClick={clickDemo} key="Africa/Lagos">Africa/Lagos</option>
+          </select>
+          <WorldClock clockData={this.state.timeZone}/>
         </header>
       </div>
     );
--- src/Components/WorldClock.js
+++ src/Components/WorldClock.js
@@ -3,16 +3,21 @@ import React from "react";
 import Clock from "./Clock";
 
 export default class WorldClock extends React.Component {
-  constructor(props) {
-    super(props)
-    
-    this.state = {
-      numberOfClock: 0
-    }
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     test: 0
+  //   }
+  // }
+
+  componentDidUpdate() {
+    console.log("Hello")
   }
 
   render() {
     let {clockData} = this.props;
+    console.log(clockData)
 
     return(
       <div>
